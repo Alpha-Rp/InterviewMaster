ea570459da541439eb6c144d76181499c9108f65
Alpha-Rp <prajwal.r.p196@gmail.com>
2025-10-04 17:13:03 +0530

    latest commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD84DpSshw6_VDkkUoLVEmgBu-n0UZbvD4",
+  authDomain: "interviewmaster-35d94.firebaseapp.com",
+  projectId: "interviewmaster-35d94",
+  storageBucket: "interviewmaster-35d94.firebasestorage.app",
+  messagingSenderId: "1027916686789",
+  appId: "1:1027916686789:web:2a9774e439aa87f04417ac",
+  measurementId: "G-1R7J8KJLMS"
 };
 
 // Initialize Firebase
